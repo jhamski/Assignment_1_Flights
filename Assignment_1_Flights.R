9ab0772a9895aa472a56fef10e4edab1051c902b
jhamski <hamski.2@gmail.com>
2015-02-01 11:34:32 -0500

    Assignment 1 for IS607 - SQL, Flights database

---
+++ Assignment_1_Flights.R
@@ -0,0 +1,3 @@
+#IS607 | Data ACQUISITION AND MANAGEMENT | ASSIGNMENT No. 1
+#James Hamski | james.hamski@spsmail.cuny.edu
+#Assignment: Write SQL queries for the Flights database to answer the assigned questions
