78952ab6f07ca4846d27ddfd8d0d873a17673de4
jhamski <hamski.2@gmail.com>
2015-02-02 19:03:10 -0500

    Added questions and one partial answer

--- Assignment_1_Flights.sql
+++ Assignment_1_Flights.sql
@@ -1,4 +1,25 @@
-﻿-- IS607 | Assignment 1 | James Hamski
+﻿-- IS607 | Data ACQUISITION AND MANAGEMENT | ASSIGNMENT No. 1
+-- James Hamski | james.hamski@spsmail.cuny.edu
 
+-- 1. what airport is furthest away?
 SELECT *
-FROM Airlines;
+FROM Planes;
+
+-- 2. What are the different numbers of engines in the planes table? For each number of engines, 
+-- which aircraft have the most number of seats?
+
+SELECT 
+DISTINCT
+engines
+FROM Planes
+
+
+-- 3. What weather conditions are associated with New York City departure delays?
+
+
+-- 4. Are older planes more likely to be delayed?
+
+
+
+-- 5. Ask (and if possible answer) a question that also requires joining information from two or more tables in the
+-- flights database, and/or assumes that additional information can be collected in advance of answering your question.
