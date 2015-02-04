39cfc3e46cb6d15d71ec9bb84911cd24b6ce73da
jhamski <hamski.2@gmail.com>
2015-02-03 23:31:27 -0500

    working on prob #1

--- Assignment_1_Flights.sql
+++ Assignment_1_Flights.sql
@@ -2,8 +2,14 @@
 -- James Hamski | james.hamski@spsmail.cuny.edu
 
 -- 1. what airport is furthest away?
-SELECT *
-FROM Planes;
+--Strategy: find the airport nearest NYC's antipode (should be Perth)
+SELECT 
+lat, lon,
+(180- 40.782070)-lat AS distLat,
+(180+73.932305)-lon AS distLon,
+faa,
+name
+FROM Airports;
 
 -- 2. What are the different numbers of engines in the planes table? For each number of engines, 
 -- which aircraft have the most number of seats?
@@ -13,6 +19,10 @@ DISTINCT
 engines
 FROM Planes
 
+SELECT
+DISTINCT 
+engines 
+FROM Planes
 
 -- 3. What weather conditions are associated with New York City departure delays?
 
