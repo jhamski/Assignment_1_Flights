bf27a932ed0def4d0dbb7167f227c52583f35303
jhamski <hamski.2@gmail.com>
2015-02-04 22:04:35 -0500

    prob 1 solved

--- Assignment_1_Flights.sql
+++ Assignment_1_Flights.sql
@@ -2,14 +2,31 @@
 -- James Hamski | james.hamski@spsmail.cuny.edu
 
 -- 1. what airport is furthest away?
---Strategy: find the airport nearest NYC's antipode (should be Perth)
+-- Easy way
+
+SELECT
+dest,
+distance
+FROM flights
+ORDER BY distance DESC
+LIMIT 10;
+
+
+-- Hard way (not the hardest way)
+--Strategy: find the airport nearest to NYC's antipode using the distance forumula. 
+-- the error induced by not doing a Great Circle calculation should be negligible if the airport is close to the antipode
+-- and there aren't other airports in the area. Luckily, NYC's antipode lies in the Indian Ocean nearest Perth, a very empty part of the world.
+-- This answers the question "what airport is the farthest away? [of all airports in database]" While the above only answers "what airport is farthest away that
+-- you can reach in a single leg? [limited by airplane range]"
+
 SELECT 
 lat, lon,
-(180- 40.782070)-lat AS distLat,
-(180+73.932305)-lon AS distLon,
+(-40.513799)-lat AS distLat,
+(105.996094)-lon AS distLon,
 faa,
 name
 FROM Airports;
+
 
 -- 2. What are the different numbers of engines in the planes table? For each number of engines, 
 -- which aircraft have the most number of seats?
@@ -25,6 +42,13 @@ engines
 FROM Planes
 
 -- 3. What weather conditions are associated with New York City departure delays?
+
+SELECT
+dep_delay, 
+precip
+FROM Flights, Weather
+ORDER BY precip DESC
+LIMIT 10;
 
 
 -- 4. Are older planes more likely to be delayed?
